7b7fa54d9a0666eb2658f46c57b0f2396abe1c41
JonathanBeltran <jbelt021@fiu.edu>
2016-04-09 12:05:53 -0400

    find function

--- app/routes/user.server.routes.js
+++ app/routes/user.server.routes.js
@@ -1,5 +1,8 @@
 var users = require('../../app/controllers/user.server.controller.js');
 
 module.exports = function(app) {
-  app.route('/user').post(users.create);
+  app.route('/user')
+  .post(users.create)
+  .get(users.list);
+
 }
